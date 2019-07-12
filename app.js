5880b83d5bdf032eb34aae4c0208ead9c5301486
voss-e <jovicevicdev@gmail.com>
2019-07-12 18:03:42 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const inputs = document.querySelectorAll('input')
+
+const visaCard = document.querySelector('')
+const patterns = {
+  visa: /^(?:4[0-9]{12}(?:[0-9]{3})?)$/,
+  mastercard: /^5[1-5]\d{14}$/,
+  discover: /^(?:6011\d{12})|(?:65\d{14})$/,
+  amex: /^3[47]\d{13,14}$/
+}
+
+function validate(field, regex) {
+  if(regex.test(field.value)) {
+    field.className = 'correct'
+    console.log('cool')
+
+  } else {
+    field.className = 'incorrect'
+  }
+}
+inputs.forEach((input) => {
+  input.addEventListener('keyup', (e) => {
+    validate(e.target, patterns[e.target.attributes.name.value])
+  })
+})
